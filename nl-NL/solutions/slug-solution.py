3b8ce4b230f346d42a8884ed522bca5dab8a4238
ninaszymor <33628387+ninaszymor@users.noreply.github.com>
2020-05-14 14:34:31 +0100

    New translations slug-solution.py (Dutch)

--- nl-NL/solutions/slug-solution.py
+++ nl-NL/solutions/slug-solution.py
@@ -4,112 +4,112 @@ from random import randint
 
 sense = SenseHat()
 
-# Variables ---------------------------
-slug = [[2,4], [3,4], [4,4]]
-white = (255, 255, 255)
-blank = (0, 0, 0)
-red = (255, 0, 0)
-direction = "right"
-vegetables = []
+# Variabelen ---------------------------
+naaktslak = [[2,4], [3,4], [4,4]]
+wit = (255, 255, 255)
+leeg = (0, 0, 0)
+rood = (255, 0, 0)
+richting = "right"
+groenten = []
 score = 0
-pause = 0.5
-dead = False
+pauze = 0,5
+dood = False
 
-# Functions ---------------------------
-def draw_slug():
-  for segment in slug:
-    sense.set_pixel(segment[0], segment[1], white)
+# Functies ---------------------------
+def teken_naaktslak():
+  for segment in naaktslak:
+    sense.set_pixel (segment[0], segment[1], wit)
 
-def move():
-  global score, pause, dead
-  remove = True
+def beweeg():
+  global score, pauze, dood
+  verwijder = True
 
-  # Find the last and first items in the slug list
-  last = slug[-1]
-  first = slug[0]
-  next = list(last) # Create a copy of the last item
+  # Zoek de laatste en eerste items in de naaktslakkenlijst
+  laatste = naaktslak[-1]
+  eerste = naaktslak[0]
+  volgende = list(laatste) # Maak een kopie van het laatste item
 
-  # Find the next pixel in the direction the slug is currently moving
-  if direction == "right":
+  # Zoek de volgende pixel in de richting waarin de slak momenteel beweegt
+  if richting == "right":
 
-    # Move along the column
-    if last[0] + 1 == 8:
-      next[0] = 0
+    # Beweeg langs de kolom
+    if laatste[0] + 1 == 8:
+      volgende[0] = 0
     else:
-      next[0] = last[0] + 1
+      volgende[0] = laatste[0] + 1
 
-  elif direction == "left":
+  elif richting == "left":
 
-    if last[0] - 1 == -1:
-      next[0] = 7
+    als laatste[0] - 1 == -1:
+      volgende[0] = 7
     else:
-      next[0] = last[0] - 1
+      volgende[0] = laatste[0] - 1
 
-  elif direction == "down":
+  elif richting == "down":
 
-    if last[1] + 1 == 8:
-      next[1] = 0
+    if laatste[1] + 1 == 8:
+      volgende[1] = 0
     else:
-      next[1] = last[1] + 1
+      volgende[1] = laatste[1] + 1
 
-  elif direction == "up":
+  elif richting == "up":
 
-    if last[1] - 1 == -1:
-      next[1] = 7
+    als laatste[1] - 1 == -1:
+      volgende[1] = 7
     else:
-      next[1] = last[1] - 1
+      volgende[1] = laatste[1] - 1
 
-  # Did I die?
-  if next in slug:
-    dead = True
+  # Ben ik doodgegaan?
+  if volgende in naaktslak:
+    dood = True
 
-  # Add this pixel at the end of the slug list
-  slug.append(next)
+  # Voeg deze pixel toe aan het einde van de naaktslakkenlijst
+  naaktslak.append(volgende)
 
-  # Set the new pixel to the slug's colour
-  sense.set_pixel(next[0], next[1], white)
+  # Stel de nieuwe pixel in op de kleur van de naaktslak
+  sense.set_pixel(volgende[0], volgende[1], wit)
 
-  if next in vegetables:
-    vegetables.remove(next)
+  if volgende in groenten:
+    groeten.remove(volgende)
     score += 1
 
     if score % 5 == 0:
-      remove = False
-      pause = pause * 0.8
+      verwijder = False
+      pauze = pauze * 0.8
 
-  if remove == True:
-    # Set the first pixel in the slug list to blank
-    sense.set_pixel(first[0], first[1], blank)
+  if verwijder == True:
+    # Stel de eerste pixel in de naaktslakkenlijst in op leeg
+    sense.set_pixel(eerste[0], eerste[1], leeg)
 
-    # Remove the first pixel from the list
-    slug.remove(first)
+    # Verwijder de eerste pixel uit de lijst
+    naaktslak.remove(eerste)
 
 
-def joystick_moved(event):
-  global direction
-  direction = event.direction
+def joystick_moved (event):
+  global richting
+  richting = event.direction
 
-def make_veg():
-  new = slug[0]
-  while new in slug:
+def maak_groente():
+  nieuw = naaktslak[0]
+  while nieuw in naaktslak:
     x = randint(0, 7)
     y = randint(0, 7)
-    new = [x, y]
-  sense.set_pixel(x, y, red)
-  vegetables.append(new)
+    nieuw = [x, y]
+  sense.set_pixel(x, y, rood)
+  groenten.append(nieuw)
 
-# Main program ------------------------
+# Hoofdprogramma ------------------------
 sense.clear()
-draw_slug()
+teken_naaktslak()
 
 sense.stick.direction_any = joystick_moved
 
-while not dead:
-  move()
-  sleep(pause)
+while not dood:
+  beweeg()
+  sleep(pauze)
 
-  # Have a 20% chance of making a veggie if there aren't many about
-  if len(vegetables) < 3 and randint(1, 5) > 4:
-    make_veg()
+  # Heb een kans van 20% om een groente te maken als er niet veel over zijn
+  if len(groenten) < 3 and randint(1, 5) > 4:
+    maak_groente()
 
 sense.show_message( str(score) )
