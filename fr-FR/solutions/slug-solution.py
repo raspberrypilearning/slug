268293f30f20d690e649a99fe746e4dd430d4eda
majamanojlovic <49232422+majamanojlovic@users.noreply.github.com>
2023-02-06 12:14:51 +0000

    New translations slug-solution.py (French)

--- fr-FR/solutions/slug-solution.py
+++ fr-FR/solutions/slug-solution.py
@@ -5,111 +5,111 @@ from random import randint
 sense = SenseHat()
 
 # Variables ---------------------------
-slug = [[2,4], [3,4], [4,4]]
-white = (255, 255, 255)
-blank = (0, 0, 0)
-red = (255, 0, 0)
+limace = [[2,4], [3,4], [4,4]]
+blanc = (255, 255, 255)
+vide = (0, 0, 0)
+rouge = (255, 0, 0)
 direction = "right"
-vegetables = []
+legumes = []
 score = 0
 pause = 0.5
-dead = False
+mort = False
 
-# Functions ---------------------------
-def draw_slug():
-  for segment in slug:
-    sense.set_pixel(segment[0], segment[1], white)
+# Fonctions ---------------------------
+def dessiner_limace():
+  for segment in limace:
+    sense.set_pixel(segment[0], segment[1], blanc)
 
-def move():
-  global score, pause, dead
-  remove = True
+def deplace():
+  global score, pause, mort
+  supprimer = True
 
-  # Find the last and first items in the slug list
-  last = slug[-1]
-  first = slug[0]
-  next = list(last) # Create a copy of the last item
+  # Trouver le dernier et le premier element de la liste limace
+  dernier = limace[-1]
+  premier = limace[0]
+  suivant = list(dernier) # Créer une copie du dernier élément
 
-  # Find the next pixel in the direction the slug is currently moving
+  # Trouver le pixel suivant dans la direction de deplacement de la limace
   if direction == "right":
 
-    # Move along the column
-    if last[0] + 1 == 8:
-      next[0] = 0
+    # Se deplacer le long de la colonne
+    if dernier[0] + 1 == 8:
+      suivant[0] = 0
     else:
-      next[0] = last[0] + 1
+      suivant[0] = dernier[0] + 1
 
   elif direction == "left":
 
-    if last[0] - 1 == -1:
-      next[0] = 7
+    if dernier[0] - 1 == -1:
+      suivant[0] = 7
     else:
-      next[0] = last[0] - 1
+      suivant[0] = dernier[0] - 1
 
   elif direction == "down":
 
-    if last[1] + 1 == 8:
-      next[1] = 0
+    if dernier[1] + 1 == 8:
+      suivant[1] = 0
     else:
-      next[1] = last[1] + 1
+      suivant[1] = dernier[1] + 1
 
   elif direction == "up":
 
-    if last[1] - 1 == -1:
-      next[1] = 7
+    if dernier[1] - 1 == -1:
+      suivant[1] = 7
     else:
-      next[1] = last[1] - 1
+      suivant[1] = dernier[1] - 1
 
-  # Did I die?
-  if next in slug:
-    dead = True
+  # Suis-je mort ?
+  if suivant in limace:
+    mort = True
 
-  # Add this pixel at the end of the slug list
-  slug.append(next)
+  # Ajouter ce pixel a la fin de la liste limace
+  limace.append(suivant)
 
-  # Set the new pixel to the slug's colour
-  sense.set_pixel(next[0], next[1], white)
+  # Definir le nouveau pixel sur la couleur de la limace
+  sense.set_pixel(suivant[0], suivant[1], blanc)
 
-  if next in vegetables:
-    vegetables.remove(next)
+  if suivant in legumes:
+    legumes.remove(suivant)
     score += 1
 
     if score % 5 == 0:
-      remove = False
+      supprimer = False
       pause = pause * 0.8
 
-  if remove == True:
-    # Set the first pixel in the slug list to blank
-    sense.set_pixel(first[0], first[1], blank)
+  if supprimer == True:
+    # Definir le premier pixel de la liste limace a vide
+    sense.set_pixel(premier[0], premier[1], vide)
 
-    # Remove the first pixel from the list
-    slug.remove(first)
+    # Supprimer le premier pixel de la liste
+    limace.remove(premier)
 
 
-def joystick_moved(event):
+def joystick_mouvement(event):
   global direction
   direction = event.direction
 
-def make_veg():
-  new = slug[0]
-  while new in slug:
+def faire_legumes():
+  nouveau = limace[0]
+  while nouveau in limace:
     x = randint(0, 7)
     y = randint(0, 7)
-    new = [x, y]
-  sense.set_pixel(x, y, red)
-  vegetables.append(new)
+    nouveau = [x, y]
+  sense.set_pixel(x, y, rouge)
+  legumes.append(nouveau)
 
-# Main program ------------------------
+# Programme principal ------------------------
 sense.clear()
-draw_slug()
+dessiner_limace()
 
-sense.stick.direction_any = joystick_moved
+sense.stick.direction_any = joystick_mouvement
 
-while not dead:
-  move()
+while not mort:
+  deplace()
   sleep(pause)
 
-  # Have a 20% chance of making a veggie if there aren't many about
-  if len(vegetables) < 3 and randint(1, 5) > 4:
-    make_veg()
+  # Avoir 20% de chance de faire un legume s'il n'y en a pas beaucoup
+  if len(legumes) < 3 and randint(1, 5) > 4:
+    faire_legumes()
 
 sense.show_message( str(score) )
